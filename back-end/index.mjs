8096a64495a403508961a85a0f38c275f8eff46f
Mattia Benevento <beneventomattia@gmail.com>
2025-10-11 09:30:05 +0200

    backend set up

---
+++ back-end/index.mjs
@@ -0,0 +1,29 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+
+// init express
+const app = express();
+const port = 3001;
+
+
+
+// if we need to use images from the public folder
+// app.use(express.static('public'));
+
+// middleware
+app.use(express.json()); // it converts json body to req.body
+app.use(morgan('dev')); // it shows log for http requests
+
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  optionsSuccessStatus: 200,
+  credentials: true  // to make cors working with credentials
+};
+
+app.use(cors(corsOptions)); // to handle cross origin requests
+
+// activate the server
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
