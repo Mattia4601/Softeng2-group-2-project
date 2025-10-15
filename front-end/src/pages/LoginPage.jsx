68fc72766fcc5c48308be2be2804f772f8015790
fatemehemmati <fahmtiti99@gmail.com>
2025-10-15 14:26:31 +0200

    Remove default options from role and counter dropdowns

--- front-end/src/pages/LoginPage.jsx
+++ front-end/src/pages/LoginPage.jsx
@@ -80,7 +80,6 @@ const LoginPage = () => {
                                 className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
                                 required
                             >
-                                <option value="">Choose a role</option>
                                 <option value="customer">Customer</option>
                                 <option value="counter">Counter Staff</option>
                             </select>
@@ -95,7 +94,6 @@ const LoginPage = () => {
                                     className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
                                     required
                                 >
-                                    <option value="">Choose a counter</option>
                                     {counters.map((counter) => (
                                         <option key={counter.counter_id} value={counter.counter_id}>
                                             {counter.counter_name}
